29d0d824ff4b530e4f4e3904be05cf0e3101638e
Billy <billyfranklim@gmail.com>
2024-02-20 23:13:39 -0300

    Removido rota não utilizada do arquivo web.php

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,4 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('App');
-    // return Inertia::render('Welcome', [
-    //     'canLogin' => Route::has('login'),
-    //     'canRegister' => Route::has('register'),
-    //     'laravelVersion' => Application::VERSION,
-    //     'phpVersion' => PHP_VERSION,
-    // ]);
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
 });
