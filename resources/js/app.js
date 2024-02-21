2be7077a4015c12136cd1df3e82a7b2ec0773dd0
Billy <billyfranklim@gmail.com>
2024-02-20 23:14:55 -0300

    Adiciona diretiva VueMaskDirective e configura ícones do Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { VueMaskDirective } from 'v-mask';
 import vuetify from "./vuetify";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -17,6 +18,7 @@ createInertiaApp({
             .use(plugin)
             .use(ZiggyVue)
             .use(vuetify)
+            .directive('mask', VueMaskDirective)
             .mount(el);
     },
     progress: {
--- resources/js/vuetify.js
+++ resources/js/vuetify.js
@@ -3,6 +3,8 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 
 const customeTheme = {
     dark: false,
@@ -39,6 +41,13 @@ const vuetify = createVuetify({
         defaultTheme: "customeTheme",
         themes: {
             customeTheme,
+        },
+    },
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
         },
     },
 });
